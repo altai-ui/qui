0392c2e1ebc83e5e52274ddab3b5a318e75c3cd6
Ahmedov Samir <s.ahmedov@maximatelecom.ru>
2019-12-13 14:35:56 +0300

    update

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,12 +1,6 @@
 const path = require('path')
 
-// Export a function. Accept the base config as the only param.
 module.exports = async ({ config, mode }) => {
-  // `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-  // You can change the configuration based on that.
-  // 'PRODUCTION' is used when building the static version of storybook.
-
-  // Make whatever fine-grained changes you need
   config.module.rules.push({
     test: /\.scss$/,
     use: [
@@ -23,7 +17,5 @@ module.exports = async ({ config, mode }) => {
     ],
     include: path.resolve(__dirname, '../')
   })
-
-  // Return the altered config
   return config
 }
