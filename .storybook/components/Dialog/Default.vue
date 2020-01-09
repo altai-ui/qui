c320404df825ad52e321c5948347200ee7e8924b
Железцов Виктор <v.zheleztsov@qvant.ru>
2020-01-09 15:24:38 +0000

    Добавление компанента Drawer

--- .storybook/components/Dialog/Default.vue
+++ .storybook/components/Dialog/Default.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="demo">
-    <el-button type="text" @click="dialogVisible = true"
-      >click to open the Dialog</el-button
-    >
+    <el-button
+      type="text"
+      @click="dialogVisible = true"
+    >click to open the Dialog</el-button>
 
     <el-dialog
       title="Tips"
@@ -11,11 +12,15 @@
       :before-close="handleClose"
     >
       <span>This is a message</span>
-      <span slot="footer" class="dialog-footer">
+      <span
+        slot="footer"
+        class="dialog-footer"
+      >
         <el-button @click="dialogVisible = false">Cancel</el-button>
-        <el-button type="primary" @click="dialogVisible = false"
-          >Confirm</el-button
-        >
+        <el-button
+          type="primary"
+          @click="dialogVisible = false"
+        >Confirm</el-button>
       </span>
     </el-dialog>
   </div>
---
+++ .storybook/components/Drawer/Default.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="demo">
+    <el-radio-group v-model="direction">
+      <el-radio label="ltr">left to right</el-radio>
+      <el-radio label="rtl">right to left</el-radio>
+      <el-radio label="ttb">top to bottom</el-radio>
+      <el-radio label="btt">bottom to top</el-radio>
+    </el-radio-group>
+
+    <el-button
+      @click="drawer = true"
+      type="primary"
+      style="margin-left: 16px;"
+    >
+      open
+    </el-button>
+
+    <el-drawer
+      append-to-body
+      title="I am the title"
+      :visible.sync="drawer"
+      :direction="direction"
+      :before-close="handleClose"
+    >
+      <span>Hi, there!</span>
+    </el-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      direction: 'rtl'
+    }
+  },
+
+  methods: {
+    handleClose(done) {
+      this.$confirm('Are you sure you want to close this?')
+        .then(_ => {
+          done()
+        })
+        .catch(_ => {})
+    }
+  }
+}
+</script>
