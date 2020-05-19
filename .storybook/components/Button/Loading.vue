fb42c785e1b944746ef7b6c8e30458f03349414c
Бочкарев Тимофей <t.bochkarev@qvant.ru>
2020-05-19 13:23:29 +0000

    Component: Tabs

--- .storybook/components/Button/Loading.vue
+++ .storybook/components/Button/Loading.vue
@@ -1,12 +1,31 @@
 <template>
   <div>
-    <el-button
-      type="primary"
-      loading
-    >Loading</el-button>
+    <el-button :loading="isLoading">Loading</el-button>
     <el-button
       plain
-      loading
+      :loading="isLoading"
     >Loading</el-button>
+    <div class="block">
+      <el-button @click="handleLoadingChange">
+        Switch Loading
+      </el-button>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Loading',
+  data() {
+    return {
+      isLoading: false
+    };
+  },
+
+  methods: {
+    handleLoadingChange() {
+      this.isLoading = !this.isLoading;
+    }
+  }
+};
+</script>
---
+++ .storybook/components/Tabs/Default.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="block">
+    <el-tabs
+      v-model="activeName"
+      @tab-click="handleClick"
+    >
+      <el-tab-pane
+        label="User"
+        name="first"
+      >Here put content</el-tab-pane>
+      <el-tab-pane
+        label="Config"
+        name="second"
+      >Here put another content</el-tab-pane>
+      <el-tab-pane
+        label="Role"
+        name="third"
+      >Awesome!</el-tab-pane>
+      <el-tab-pane
+        label="Task"
+        name="fourth"
+        disabled
+      >Task</el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      activeName: 'first'
+    };
+  },
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab, event);
+    }
+  }
+};
+</script>
