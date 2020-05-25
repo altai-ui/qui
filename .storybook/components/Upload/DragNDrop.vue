0c7bc554463ca797e3cdacb692c223e55966f9b3
Бочкарев Тимофей <t.bochkarev@qvant.ru>
2020-05-25 12:52:44 +0000

    Component: Upload

---
+++ .storybook/components/Upload/DragNDrop.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="block">
+    <span class="demonstration">For el-upload class 'upload' is required </span>
+    <el-upload
+      :class="classes"
+      drag
+      action="https://jsonplaceholder.typicode.com/posts/"
+      :on-change="handleChange"
+      :on-error="handleError"
+      :file-list="fileList"
+    >
+      <i class="q-icon-cloud-upload-fill"></i>
+      <div class="el-upload__text">загрузить файл</div>
+    </el-upload>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      error: false
+    };
+  },
+  computed: {
+    classes() {
+      return {
+        upload: true,
+        error: this.error
+      };
+    }
+  },
+  methods: {
+    handleChange(file, fileList) {
+      console.log(file, fileList);
+    },
+    handleError(error) {
+      console.log(error);
+      this.error = true;
+    }
+  }
+};
+</script>
