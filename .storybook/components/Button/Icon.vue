33513452b53e2cd1d7c2492d7db75e57c2b13b9d
Tim Bochkarev <t.bochkarev@qvant.ru>
2020-06-02 18:44:59 +0300

    change button

--- .storybook/components/Button/Icon.vue
+++ .storybook/components/Button/Icon.vue
@@ -1,20 +1,50 @@
 <template>
   <div>
-    <el-button
-      type="primary"
-      icon="q-icon-search"
-    >Q-icon search</el-button>
-    <el-button plain>
-      El-icon upload
-      <i class="el-icon-upload el-icon-right" />
-    </el-button>
-    <el-button
-      icon="el-icon-share"
-      plain
-    />
-    <el-button
-      circle
-      icon="q-icon-question"
-    />
+    <div class="block">
+      <span
+        class="demonstration"
+      >add class <b>icon</b> to button (default size 24x24)</span>
+      <el-row>
+        <el-button
+          icon="q-icon-pic"
+          class="icon"
+        ></el-button>
+        <el-button
+          plain
+          class="icon"
+          icon="q-icon-cloud-upload"
+        > </el-button>
+        <el-button
+          class="icon"
+          circle
+          icon="q-icon-question-mark"
+        />
+      </el-row>
+    </div>
+    <span
+      class="demonstration"
+    >add prop <b>size="medium"</b> to scale icon button to 40x40</span>
+    <div class="block">
+      <el-row>
+        <el-button
+          icon="q-icon-pic"
+          class="icon"
+          size="medium"
+        ></el-button>
+        <el-button
+          plain
+          class="icon"
+          size="medium"
+        >
+          <i class="q-icon-cloud-upload" />
+        </el-button>
+        <el-button
+          class="icon"
+          circle
+          size="medium"
+          icon="q-icon-question-mark"
+        />
+      </el-row>
+    </div>
   </div>
 </template>
