4666cfdc9f1374e357494e7ede4c5b801626861f
Алисултанов Шамиль <sh.alisultanov@qvant.ru>
2020-07-13 13:50:16 +0000

    Компонент QTabs

---
+++ src/qComponents/QTabPane/src/QTabPane.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="q-tab-pane">
+    <div class="q-tab-pane__inner">
+      <button
+        class="q-tab-pane__btn"
+        :class="getTabBtnClasses"
+        :style="tabWidthStyle"
+        :disabled="isDisabled"
+        @click="handleTabClick"
+      >
+        {{ title }}
+      </button>
+      <slot name="content" />
+    </div>
+    <div
+      v-if="description"
+      class="q-tab-pane__description"
+      :style="tabWidthStyle"
+    >
+      {{ description }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QTabPane',
+
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    description: {
+      type: String,
+      default: ''
+    },
+    width: {
+      type: [String, Number],
+      default: ''
+    }
+  },
+
+  computed: {
+    isDisabled() {
+      return this.disabled || this.$parent.disabled;
+    },
+
+    tabWidthStyle() {
+      const sourceWidthValue = this.width || this.$parent.tabWidth;
+
+      return {
+        width: Number(sourceWidthValue)
+          ? `${Number(sourceWidthValue)}px`
+          : sourceWidthValue
+      };
+    },
+
+    getTabBtnClasses() {
+      return {
+        'q-tab-pane__btn_active': this.$parent.currentName === this.name,
+        'q-tab-pane__btn_disabled': this.isDisabled
+      };
+    }
+  },
+
+  created() {
+    // eslint-disable-next-line no-underscore-dangle
+    const parentComponentName = this.$parent.$options._componentTag;
+
+    if (
+      parentComponentName !== 'q-tabs' &&
+      process.env.NODE_ENV !== 'production'
+    ) {
+      console.error('QTabPane: component parent must be q-tabs');
+    }
+  },
+
+  methods: {
+    handleTabClick() {
+      this.$parent.updateValue(this.name);
+    }
+  }
+};
+</script>
---
+++ src/qComponents/QTabs/src/QTabs.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="q-tabs">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QTabs',
+
+  model: {
+    prop: 'value',
+    event: 'change'
+  },
+
+  props: {
+    tabWidth: {
+      type: [String, Number],
+      default: ''
+    },
+    value: {
+      type: String,
+      default: ''
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  data() {
+    return {
+      currentName: ''
+    };
+  },
+
+  watch: {
+    value: {
+      handler(name) {
+        this.updateValue(name);
+      },
+      immediate: true
+    }
+  },
+
+  methods: {
+    updateValue(name) {
+      this.$emit('change', name);
+      this.currentName = name;
+    }
+  }
+};
+</script>
