504d03d089491a8a1ad6eddca4b0d0579d5b7dcf
Новиков Максим <m.novikov@qvant.ru>
2020-08-13 08:59:24 +0000

    Component: QBreadcrumbs

---
+++ .storybook/qComponents/QBreadcrumbs/Default.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div class="block">
+      <span class="demonstration">Default Breadcrumbs</span>
+      <q-breadcrumbs postfix="[postfix]" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+
+Object.defineProperty(Vue.prototype, '$route', {
+  get() {
+    return {
+      matched: [
+        {
+          path: 'path-a',
+          name: 'Роут a',
+          meta: {
+            breadcrumb: 'Роут a'
+          }
+        },
+        {
+          path: 'path-b',
+          name: 'Очень длинный роут b',
+          meta: {
+            breadcrumb: 'Роут b'
+          }
+        },
+        {
+          path: 'path-c',
+          name: 'Роут c',
+          meta: {
+            breadcrumb: 'Роут c'
+          }
+        },
+        {
+          path: 'path-d',
+          name: 'Роут d',
+          meta: {
+            breadcrumb: 'Роут d'
+          }
+        }
+      ]
+    };
+  }
+});
+
+export default {};
+</script>
---
+++ src/qComponents/QBreadcrumbs/index.js
@@ -0,0 +1,8 @@
+import QBreadcrumbs from './src/QBreadcrumbs.vue';
+
+/* istanbul ignore next */
+QBreadcrumbs.install = function(Vue) {
+  Vue.component(QBreadcrumbs.name, QBreadcrumbs);
+};
+
+export default QBreadcrumbs;
