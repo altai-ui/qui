9a5777c03f3fc0721e7d07767b13c7aa911da504
Новиков Максим <m.novikov@qvant.ru>
2020-08-27 14:15:24 +0000

    Feat/datepicker

--- .storybook/components/DatePicker/Disabled.vue
+++ .storybook/components/DatePicker/Disabled.vue
@@ -29,37 +29,8 @@
 export default {
   data() {
     return {
-      pickerOptions: {
-        disabledDate(time) {
-          return time.getTime() > Date.now();
-        },
-        shortcuts: [
-          {
-            text: 'Today',
-            onClick(picker) {
-              picker.$emit('pick', new Date());
-            }
-          },
-          {
-            text: 'Yesterday',
-            onClick(picker) {
-              const date = new Date();
-              date.setTime(date.getTime() - 3600 * 1000 * 24);
-              picker.$emit('pick', date);
-            }
-          },
-          {
-            text: 'A week ago',
-            onClick(picker) {
-              const date = new Date();
-              date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
-              picker.$emit('pick', date);
-            }
-          }
-        ]
-      },
       value1: '',
-      value2: ''
+      value2: [new Date(), new Date()]
     };
   }
 };
